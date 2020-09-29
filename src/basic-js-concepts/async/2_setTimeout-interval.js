9c8dd2fafd0f41787069441135e6f0668d138b86
silvana <silvana.belen.garcia@gmail.com>
2020-09-29 18:38:10 -0300

    Async - setTimeout vs setInterval

---
+++ src/basic-js-concepts/async/2_setTimeout-interval.js
@@ -0,0 +1,21 @@
+/*------------- setTimeout VS setInterval -------------*/ console.log('setTimeout vs. setInterval');
+// setTimeout: calls a function after a time interval in milliseconds (it executes just once)
+let timeoutID = setTimeout(() => {
+  console.log("Oh Yeaaah! It has passed 3000 milliseconds, I'm the setTimeout");
+  clearTimeout(timeoutID); // Will clear the timer.
+}, 3000); // Will alert once, after a second.
+
+//setInterval: calls a function n times, every x milliseconds
+let counter = 0,
+  intervalID = setInterval(() => {
+    console.log("Oh Yeaaah! It has passed 1000 milliseconds, I'm the setInterval");
+    counter++;
+    if (counter === 5) {
+      clearInterval(intervalID); // Will clear the timer, or stop it
+    }
+  }, 1000); // Will alert every second.
+
+// ADVICE: It’s always good to clear your timer function after you’re done using it.
+
+// GOOD QUESTION: Why not to use setInterval with one execution, instead of setTimeout??
+// Cause purposes are just different and besides, you'd need to add an if condition and a clearInterval() to make it stop.
