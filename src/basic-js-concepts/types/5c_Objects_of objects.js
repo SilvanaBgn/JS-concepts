2fb98f00ef3e2b619602625003527119114b9712
silvana <silvana.belen.garcia@gmail.com>
2020-10-08 13:41:55 -0300

    T001 Objects of objects

---
+++ src/basic-js-concepts/types/5c_Objects_of objects.js
@@ -0,0 +1,4 @@
+/*
+-->Analizar qué sucede con un array/objeto de valores primitivos (string por ej) VS. array/object de objetos
+-->Cómo reaccionan operadores como map/filter/reduce/concat/for/forEach -algunos de ellos, no es necesario probar con todos- : con array de objetos VS con array de string.
+*/
