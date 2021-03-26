706164b15ff5660eb026289d6745bb5750e5fb8a
silvana <silvana.belen.garcia@gmail.com>
2021-03-26 16:55:14 -0300

    T007 Example for __proto__ vs prototype

---
+++ src/basic-js-concepts/prototype/__proto__/in-the-browser.js
@@ -0,0 +1,18 @@
+// - To execute this code in the browser console -
+//-----------------------------------
+/*function Dog(pName) {
+  this.name = pName;
+  return `Hello ${this.name}`;
+}
+
+Dog.prototype.breed = 'Bulldog';
+
+Dog.prototype
+
+let myDoggie = new Dog('Milton');
+
+myDoggie
+
+myDoggie.prototype
+myDoggie.__proto__
+*/
